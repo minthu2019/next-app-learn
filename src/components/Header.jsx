1f318c28147b08f5f7d81a8e161978eb4caaf274
Min Thu Khaing <mtk.min2003@gmail.com>
2024-09-17 22:36:52 +0630

    Add auth using clerk

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,40 +1,59 @@
-"use client"
-import Link from 'next/link'
-import { usePathname } from 'next/navigation'
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import {
+  UserButton,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+} from "@clerk/nextjs";
 
 const links = [
-    {
-        title : "Home" ,
-        url : "/" ,
-    },
-    {
-        title : "About" ,
-        url : "/about" ,
-    },
-    {
-        title : "SignIn" ,
-        url : "/signIn" ,
-    },
-]
+  {
+    title: "Home",
+    url: "/",
+  },
+  {
+    title: "About",
+    url: "/about",
+  },
+];
 
 const Header = () => {
-    const pathname = usePathname();
+  const pathname = usePathname();
   return (
-    <header >
-      <div className=' flex items-center justify-between bg-gradient-to-r from-blue-400 to-pink-400 py-2 px-20'>
-        <span className=' text-2xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-blue-800 to-pink-800 hover:from-blue-600 hover:to-pink-600'>AuthApp</span>
+    <header>
+      <div className=" flex items-center justify-between bg-gradient-to-r from-blue-400 to-pink-400 py-2 px-20">
+        <span className=" text-2xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-blue-800 to-pink-800 hover:from-blue-600 hover:to-pink-600">
+          AuthApp
+        </span>
         <nav>
-            <ul className=' flex items-center justify-center gap-4 '>
-                { links.map((link)=> {
-                    return (
-                        <Link href={link.url} className={`hover:text-gray-500 font-bold ${ pathname === link.url ? " text-white" : " "}`}>{link.title}</Link>
-                    )
-                })}
-            </ul>
+          <ul className=" flex items-center justify-center gap-4 ">
+            {links.map((link) => {
+              return (
+                <Link
+                  href={link.url}
+                  className={`hover:text-gray-500 font-bold ${
+                    pathname === link.url ? " text-white" : " "
+                  }`}
+                >
+                  {link.title}
+                </Link>
+              );
+            })}
+            <div className=" flex items-center justify-center">
+              <SignedOut>
+                <SignInButton />
+              </SignedOut>
+              <SignedIn>
+                <UserButton />
+              </SignedIn>
+            </div>
+          </ul>
         </nav>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
